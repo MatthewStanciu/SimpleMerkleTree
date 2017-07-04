9e1a215a4ccdebc05d11ccd9010e235dccb20b45
TechBug2012 <mattbstanciu@gmail.com>
2017-07-04 16:43:18 +0300

    Edited comments

--- mtree.py
+++ mtree.py
@@ -15,6 +15,7 @@ def create_tree(hash_list):
     # Starting at the first entry, through each entry in the hash list, grouping every 2 entries
     for index in range(0, len(hash_list)-1, 2):
 
+        # If the length of the item isn't 64 (the length of a sha256 hash), turn it into a sha256 hash
         if len(list(hash_list[index])) != 64:
             hash_list = turn_into_hash(hash_list)
 
@@ -23,11 +24,11 @@ def create_tree(hash_list):
 
         child_hash_list.append(left + right)  # Group the hashes
 
-    if len(hash_list) % 2 == 1:  # If there is an odd number, keep hashing the last item to itself
+    if len(hash_list) % 2 == 1:  # If there is an odd number of items in the list, keep hashing the last item to itself
         child_hash_list.append(hash_list[-1])  # Append the last element
 
-    if len(hash_list) == 1:
+    if len(hash_list) == 1:  # If the Merkle root has been reached, stop calling the function & return the root hash
         print("Merkle root:")
         return hash_list[0]
 
-    return create_tree(child_hash_list)  # Keep calling this function until the hash list length is 1
+    return create_tree(child_hash_list)  # Recursively call this function until the hash list length is 1
